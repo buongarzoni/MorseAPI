5848de19b790710601d802e7f36e9d3362e2619e
christian buongarzoni <christian.buongarzoni@gmail.com>
2021-02-25 15:20:35 -0300

    fix heroku

--- src/main/java/com/translator/morseapi/domain/model/Alphabets.java
+++ src/main/java/com/translator/morseapi/domain/model/Alphabets.java
@@ -1,13 +1,33 @@
 package com.translator.morseapi.domain.model;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.translator.morseapi.exceptions.ErrorInJSONFileException;
 import com.translator.morseapi.util.ParseJSONFromFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 
 public class Alphabets {
 
-    public static final HashMap<String, String> LATIN_TO_MORSE = ParseJSONFromFile.jsonFileToHashMap("src\\main\\resources\\alphabets\\LatinToMorse.json");
+    public static final HashMap<String, String> LATIN_TO_MORSE;
+    public static final HashMap<String, String> MORSE_TO_LATIN;
 
-    public static final HashMap<String, String> MORSE_TO_LATIN = ParseJSONFromFile.jsonFileToHashMap("src\\main\\resources\\alphabets\\MorseToLatin.json");
+    static {
+        try {
+            LATIN_TO_MORSE = ParseJSONFromFile.jsonFileToHashMap(ResourceUtils.getFile("classpath:alphabets/LatinToMorse.json").toPath().toString());
+        } catch (Exception e) {
+            throw new ErrorInJSONFileException(e.toString());
+        }
+    }
+
+    static {
+        try {
+            MORSE_TO_LATIN = ParseJSONFromFile.jsonFileToHashMap(ResourceUtils.getFile("classpath:alphabets/MorseToLatin.json").toPath().toString());
+        } catch (Exception e) {
+            throw new ErrorInJSONFileException(e.toString());
+        }
+    }
 }
